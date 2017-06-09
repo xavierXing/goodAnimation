450a1d533c286977a6ad79f803763f0ed9c5d13b
邢浩 <xxxinghao@sina.com>
2017-06-09 13:44:57 +0800

    点赞动画
    点赞动画Swift

---
+++ GoodAnimaton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAnimaton
+//
+//  Created by 邢浩 on 2017/6/9.
+//  Copyright © 2017年 邢浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
